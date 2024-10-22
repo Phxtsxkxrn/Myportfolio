842694b171343785de00ed4fbe51a04d435737fa
Phatsakorn✅ <epsk.coin@gmail.com>
2024-10-22 08:36:40 +0700

    About

---
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I'm Phatsakorn</h1>
+                <p className={styles.description}>
+                    I'm a 4th-year Computer Engineering and Information Technology student at Kasetsart University,
+                    currently interning as part of a cooperative education program.
+                    Feel free to reach out if you'd like to connect or learn more!
+                </p>
+                <a href="mailto:epsk.coin@gmail.com" className={styles.contactBtn}>
+                    Contact Me
+                </a>
+            </div>
+            <img
+                src={getImageUrl("hero/heroImage1.png")}
+                alt="Hero image of me"
+                className={styles.heroImg}
+            />
+            <div className={styles.topBlur}/>
+            <div className={styles.bottomBlur}/>
+        </section>
+    );
+};
