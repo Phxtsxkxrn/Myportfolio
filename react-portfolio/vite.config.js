ee2ba02a5108cd93fb4ee984c636a915f9d6825d
Phxtsxkxrn <epsk.coin@gmail.com>
2025-06-04 22:46:53 +0700

    netily 18

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +8,10 @@ export default defineConfig({
     modules: {
       localsConvention: "camelCase",
     },
+  },
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
 });
